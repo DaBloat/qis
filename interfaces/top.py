1eaa1a4ce4074496706ba323f371dfe843fbede1
Kurt <134671520+DaBloat@users.noreply.github.com>
2024-11-20 21:55:06 +0800

    Day 3: Some additions (not good)

--- interfaces/top.py
+++ interfaces/top.py
@@ -9,4 +9,26 @@ class Top(QFrame):
         self.height = 115
         self.setMinimumSize(QSize(self.width, self.height))
         self.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
-        self.setStyleSheet("background:rgb(255, 0, 255);")
+        self.setStyleSheet("""background:rgb(255, 255, 255);
+                              """)
+
+        self.layout = QHBoxLayout(self)
+
+        self.title = QFrame(self)
+        self.title.setObjectName('title')
+        self.title.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Expanding)
+        self.title.setMinimumSize(50,0)
+        self.title.setStyleSheet("""QFrame#title{background: rgb(255,255,255);
+                                    border-style:solid;
+                                    border-width: 2px;}""")
+        self.title_layout = QVBoxLayout(self.title)
+        self.text = QLabel("QIS")
+        self.text.setAlignment(Qt.AlignCenter)
+        self.text.setStyleSheet('font: 87 48pt "Arial Black";\ncolor: rgb(234, 196, 163);')
+        self.text2 = QLabel("Quantum Interactive Space")
+        self.title_layout.addWidget(self.text)
+        self.title_layout.addWidget(self.text2)
+        self.layout.addWidget(self.title)
+
+        # self.button = QPushButton(self)
+        # self.layout.addWidget(self.button)
