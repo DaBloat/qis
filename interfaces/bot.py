ceeed539610ead3d2f0ff766843e5da0f93f4f5a
Kurt <134671520+DaBloat@users.noreply.github.com>
2024-11-21 23:54:38 +0800

    Day 4: Bottom Layer added design
    Another Spaghetti for now

--- interfaces/bot.py
+++ interfaces/bot.py
@@ -9,6 +9,27 @@ class Bottom(QFrame):
         self.scene = recipient
         self.width = 0 # It automatically scales in the Screen
         self.height = 130
+        self.setObjectName("bottomFrame")
         self.setMinimumSize(QSize(self.width, self.height))
         self.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
-        self.setStyleSheet("background: rgb(170, 0, 0);")
+        self.setStyleSheet("QFrame#bottomFrame{background:rgb(234, 196, 163);}")
+
+        self.layout = QHBoxLayout(self)
+
+        self.vertlayout = QVBoxLayout(self)
+        
+        self.button1 = QPushButton('qubit')
+        self.button2 = QPushButton('operators')
+        self.button3 = QPushButton('functions')
+        self.button4 = QPushButton('gates')
+
+        for i in [self.button1,self.button2, self.button3, self.button4]:
+            self.vertlayout.addWidget(i)
+
+        self.layout.addLayout(self.vertlayout)
+
+        self.scrollarea = QScrollArea(self)
+        self.scrollarea.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.scrollarea.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
+        self.layout.addWidget(self.scrollarea)
+
